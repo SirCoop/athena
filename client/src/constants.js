d3ecc51b9892960a4e9d828267b84f99f20efaba
Gary Cooper <cooper.garym@gmail.com>
2019-03-02 13:07:29 -0500

    removed client source.

--- client/src/constants.js
+++
@@ -1,5 +0,0 @@
-const CONSTANTS = {
-  API_ROOT: process.env.NODE_ENV === 'development' ? process.env.DEV_API_ROOT : process.env.PROD_API_ROOT,
-};
-
-export default CONSTANTS;
--- client/src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import toggleSidebar from './toggleSidebar';
-
-export default combineReducers({
-  isSidebarOpen: toggleSidebar,
-});
